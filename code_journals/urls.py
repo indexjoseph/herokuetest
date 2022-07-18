bcc21c898dea44a98d5c45895928d9c56718dbc1
jaoladeji <jaoladeji1@cataomount.wcu.edu>
2022-07-18 14:24:14 -0400

    f

--- code_journals/urls.py
+++ code_journals/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name = "index"),
 
     #Show all topics.
-    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>', views.topics, name='topics'),
 ]
